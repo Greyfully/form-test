53e16c2e2d0329ba8f9bb9ec37db7e11f8dafe5e
Greyfully <82115949+Greyfully@users.noreply.github.com>
2021-04-08 18:08:14 -0500

    Request #2 - Subscription Options Update
    "Grey Stepp" option added to field  5033995 via the API.
    
    This commit updates the form to reflect this new option.
    
    Screenshots added to /screenshots showing that the field is working and amends the member record.

--- blocks/form.php
+++ blocks/form.php
@@ -104,6 +104,15 @@
                         <p class="text-gray-500">Get sent Spam!</p>
                      </div>
                      </div>
+                     <div class="flex items-start">
+                     <div class="flex items-center h-5">
+                        <input id="subscriptions_Grey-Stepp" value="Grey Stepp" name="grey-stepp" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
+                     </div>
+                     <div class="ml-3 text-sm">
+                        <label for="spam" class="font-medium text-gray-700">Grey Stepp</label>
+                        <p class="text-gray-500">All the Grey Stepp news you could ever want!</p>
+                     </div>
+                     </div>
                   </div>
                </fieldset>
                </div>
